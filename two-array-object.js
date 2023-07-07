f1151bd12e9015905491ec2bb6b7a41930a678d1
Tichin Wu <twu38@mail.ccsf.edu>
2023-07-07 16:51:05 -0700

    two-array-object.js done

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,21 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+/** two input arrays, using the values of the first array as keys,
+ * and the values of the second array as values and make an object.
+ * Then return the object.
+ * If there are more keys than values, value should be null.
+ * If there are more values than keys,value should be ignored
+*/
+
+
+function twoArrayObject(keys, values) {
+
+  let obj = {};
+
+  for (let i = 0; i < keys.length; i++) {
+
+    obj[keys[i]] = values[i] ? values[i] : null;
+
+  }
+
+  return obj;
 }
