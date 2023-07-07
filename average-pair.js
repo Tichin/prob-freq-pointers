a07b08edaeef4d4b3ad345a4d5055ba6d528e84b
Kirra Hyde <kirra.hyde@gmail.com>
2023-07-07 19:09:52 -0400

    did average pair

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,38 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/**Takes sorted array of nums.  Takes a target average.
+ * Ex. [1,2,3], 2.5 // true
+ * Complexity: O(n)
+ * Could be empty array
+*/
+
+function averagePair(sortedNums, targetAvg) {
+
+  //make indexes for first and last nums in array.
+
+  //while loop last target > first target
+  //if they're equal, return true
+  //if avg of indices is less than target, last target--
+  //if avg of indices is more than target, first target++
+
+  let firstIndex = 0;
+  let lastIndex = sortedNums.length -1;
+  let avg = 0;
+  if (sortedNums.length === 0) {
+    return false;
+  }
+  while (lastIndex > firstIndex) {
+    avg = (sortedNums[firstIndex] + sortedNums[lastIndex]) / 2;
+    if (avg === targetAvg) {
+      return true;
+    } else if (avg < targetAvg) {
+      firstIndex++;
+    } else if (avg > targetAvg) {
+      lastIndex--;
+    }
+
+  }
+  return false;
+
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,10 +1,11 @@
-// add whatever parameters you deem necessary & write doc comment
+"use strict";
+//** add whatever parameters you deem necessary & write doc comment
 // input  2 strings word letter,
 // true if word can be build by the letters,otherwise false
 // only lowercase letters
 // O(w+k)//O(n)
 // letters 'abc' word 'ba' true
-// letters 'abc' word 'aa' false
+// letters 'abc' word 'aa' false*/
 
 //  build freq counter for letters, another one for word
 function canConstructWord(word, letters) {
